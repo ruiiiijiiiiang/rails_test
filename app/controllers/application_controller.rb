05c6251a7412553f7773bfda9a231757bc345b8c
Rui Jiang <me@ruijiang.me>
2018-08-13 22:17:08 -0700

    finished toturial app from railstutorial.org

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello, world!"
+  end
+
+  def yolo
+    render html: "Yolo, swag!"
+  end
 end
